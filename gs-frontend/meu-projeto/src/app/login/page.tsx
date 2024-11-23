17f5d6c6e87ae21efc038098fd8f7133eb36c591
livialopes55 <162389190+livialopes55@users.noreply.github.com>
2024-11-22 21:10:22 -0300

    Update page.tsx
    Alterei algumas informações

--- gs-frontend/meu-projeto/src/app/login/page.tsx
+++ gs-frontend/meu-projeto/src/app/login/page.tsx
@@ -28,7 +28,7 @@ const Login = () => {
 
     // Validação dos campos
     if (!email || !isValidEmail(email)) {
-      setError('Por favor, insira um email válido.');
+      setError('Por gentileza, insira um email válido.');
       return;
     }
 
